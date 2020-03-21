64b2c6b81e13ed20479501e3e448e81ad7dcacf0
Benedikt Gregor <runbgn@gmail.com>
2020-03-21 19:20:32 +0100

    combine store modules

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,27 @@
-import store from './store/index.js'; 
+import { Store } from "./store/index.js";
 
 // Load up components
-import Count from './components/count.js';
-import List from './components/list.js';
-import Status from './components/status.js';
+import Count from "./components/count.js";
+import List from "./components/list.js";
+import Status from "./components/status.js";
 
 // Load up some DOM elements
-const formElement = document.querySelector('.js-form');
-const inputElement = document.querySelector('#new-item-field');
+const formElement = document.querySelector(".js-form");
+const inputElement = document.querySelector("#new-item-field");
 
 // Add a submit event listener to the form and prevent it from posting back
-formElement.addEventListener('submit', evt => {
-    evt.preventDefault();
-    
-    // Grab the text value of the textbox and trim any whitespace off it
-    let value = inputElement.value.trim();
-    
-    // If there's some content, trigger the action and clear the field, ready for the next item
-    if(value.length) {
-        store.dispatch('addItem', value);
-        inputElement.value = '';
-        inputElement.focus();
-    }
+formElement.addEventListener("submit", evt => {
+  evt.preventDefault();
+
+  // Grab the text value of the textbox and trim any whitespace off it
+  let value = inputElement.value.trim();
+
+  // If there's some content, trigger the action and clear the field, ready for the next item
+  if (value.length) {
+    Store.dispatch("addItem", value);
+    inputElement.value = "";
+    inputElement.focus();
+  }
 });
 
 // Instantiate components
