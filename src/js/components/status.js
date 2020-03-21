ebae89114af0feb93f4050c251ddb98c5fd78ddd
Benedikt Gregor <runbgn@gmail.com>
2020-03-21 18:57:36 +0100

    replace self /w this

--- src/js/components/status.js
+++ src/js/components/status.js
@@ -1,23 +1,22 @@
-import Component from '../lib/component.js';
-import store from '../store/index.js';
+import Component from "../lib/component.js";
+import store from "../store/index.js";
 
 export default class Status extends Component {
-    constructor() {
-        super({
-            store,
-            element: document.querySelector('.js-status')
-        });
-    }
-    
-    /**
-     * React to state changes and render the component's HTML
-     *
-     * @returns {void}
-     */
-    render() {
-        let self = this;
-        let suffix = store.state.items.length !== 1 ? 's' : '';
-        
-        self.element.innerHTML = `${store.state.items.length} item${suffix}`;
-    }
+  constructor() {
+    super({
+      store,
+      element: document.querySelector(".js-status")
+    });
+  }
+
+  /**
+   * React to state changes and render the component's HTML
+   *
+   * @returns {void}
+   */
+  render() {
+    let suffix = store.state.items.length !== 1 ? "s" : "";
+
+    this.element.innerHTML = `${store.state.items.length} item${suffix}`;
+  }
 }
